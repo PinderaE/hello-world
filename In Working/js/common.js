267a9a6a678cbdf334695cf519df41a65ad5b9bc
Evgeniy Pindera <pindera91@gmail.com>
2018-02-21 14:29:29 +0200

    work

--- In Working/js/common.js
+++ In Working/js/common.js
@@ -1,4 +1,16 @@
+$(function(){
+    
+    $('.accordion__head').on('click', function(){
+	var el = $(this);
+	el.next('.accordion__body').slideToggle();
+	el.toggleClass('open');
+	return false;
+	});
 
+
+
+
+});
 
 
         
