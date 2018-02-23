4830d01920dbd28b13f142d0f1d9ec8521a8bd58
Evgeniy Pindera <pindera91@gmail.com>
2018-02-23 15:12:46 +0200

    add sidebar and footer

--- In Working/js/common.js
+++ In Working/js/common.js
@@ -1,5 +1,4 @@
 $(function(){
-    
     $('.accordion__head').on('click', function(){
 	var el = $(this);
 	el.next('.accordion__body').slideToggle();
@@ -9,7 +8,20 @@ $(function(){
 
 
     $('.styler').styler();
-
+    
+    
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 1000,
+      values: [ 175, 500 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] );
+        $( "#amount1" ).val(  "$" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
 });
 
 
