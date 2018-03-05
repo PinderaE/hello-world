c80f50bd4f747367aac5a5932461001aa66ac2eb
Evgeniy Pindera <pindera91@gmail.com>
2018-03-05 15:42:13 +0200

    new

--- In Working/js/common.js
+++ In Working/js/common.js
@@ -1,30 +1,39 @@
-$(function(){
-    $('.accordion__head').on('click', function(){
-	var el = $(this);
-	el.next('.accordion__body').slideToggle();
-	el.toggleClass('open');
-	return false;
-	});
+$(function() {
+
+    $(window).resize(function() {
+        if ($(window).width() <= '960') {
+            $("#logo").attr("src", "img/logo-sm.png");
+        } else {
+            $("#logo").attr("src", "img/logo.png");
+        }
+
+    });
+
+
+
+    $('.accordion__head').on('click', function() {
+        var el = $(this);
+        el.next('.accordion__body').slideToggle();
+        el.toggleClass('open');
+        return false;
+    });
 
 
     $('.styler').styler({
         selectPlaceholder: 'Size',
     });
-    
-    
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 1000,
-      values: [ 175, 500 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] );
-        $( "#amount1" ).val(  "$" + ui.values[ 1 ] );
-      }
+
+
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 1000,
+        values: [175, 500],
+        slide: function(event, ui) {
+            $("#amount").val("$" + ui.values[0]);
+            $("#amount1").val("$" + ui.values[1]);
+        }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
-    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+    $("#amount").val("$" + $("#slider-range").slider("values", 0));
+    $("#amount1").val("$" + $("#slider-range").slider("values", 1));
 });
-
-
-        
